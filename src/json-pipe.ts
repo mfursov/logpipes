0c35c02d88f4abe4bebddd4045af39726503e339
Mikhail Fursov <mike.fursov@gmail.com>
2023-07-01 16:00:45 -0500

    Better support of JS types in json-pipe

--- src/json-pipe.ts
+++ src/json-pipe.ts
@@ -16,7 +16,7 @@ export const DEFAULT_JSON_PIPE_OPTIONS: Readonly<JsonPipeOptions> = {
     maxArrayLength: 100,
     maxObjectEntries: 100,
     isTopLevelProperty: propertyName => propertyName.startsWith('@'),
-    getObjectArgumentMessageToken: argumentIndex => `$${argumentIndex}`,
+    getObjectArgumentMessageToken: argumentIndex => `$${argumentIndex + 1}`,
     undefinedMessageValue: undefined,
 };
 
@@ -27,25 +27,24 @@ export const DEPTH_LIMIT_VALUE = '[Depth limit ~]';
 export const ARRAY_LENGTH_LIMIT_VALUE = '[Array, length: $length ~]';
 export const OBJECT_ENTRY_LIMIT_VALUE = '[Object, entries: $count ~]';
 
-export function createJsonPipe(options: Partial<JsonPipeOptions>): LogPipe {
+export function createJsonPipe(options: Partial<JsonPipeOptions> = {}): LogPipe {
     const pipeOptions: JsonPipeOptions = {...DEFAULT_JSON_PIPE_OPTIONS, ...options};
-    return (_, ...args) => {
+    return (type, ...args) => {
         const result: Record<string, unknown> = {};
         let message: string | undefined = undefined;
-        for (let i = 0; i < args.length; i++) {
-            const arg = args[i];
+        result[pipeOptions.messagePropertyName] = undefined; // Set it first, so it will be the first property in JSON.
+        let messageArgIndex = 0;
+        for (let argIndex = 0; argIndex < args.length; argIndex++) {
+            const arg = simplifyType(args[argIndex]);
             let messageToken = arg;
-            if (typeof arg === 'function') {
-                messageToken = FUNCTION_VALUE;
-            } else if (typeof arg === 'symbol') {
-                messageToken = SYMBOL_VALUE;
-            } else if (typeof arg === 'object') {
+            if (typeof arg === 'object' && arg !== null) {
                 const topLevelProperties = pickTopLevelProperties(arg, pipeOptions);
-                for (const entry of Object.entries(topLevelProperties)) {
-                    result[entry[0]] = entry[1];
+                for (const [topLevelPropertyName, topLevelPropertyValue] of Object.entries(topLevelProperties)) {
+                    result[topLevelPropertyName] = topLevelPropertyValue;
                 }
-                messageToken = pipeOptions.getObjectArgumentMessageToken(i, arg);
-                result[messageToken] = buildSafeJsonValue(arg, pipeOptions);
+                messageToken = pipeOptions.getObjectArgumentMessageToken(messageArgIndex, arg);
+                result[messageToken] = convertToSafeJson(arg, pipeOptions);
+                messageArgIndex++;
             } else if (arg === undefined) {
                 if (pipeOptions.undefinedMessageValue !== undefined) {
                     messageToken += pipeOptions.undefinedMessageValue;
@@ -53,12 +52,12 @@ export function createJsonPipe(options: Partial<JsonPipeOptions>): LogPipe {
             } else {
                 messageToken = arg;
             }
-            message = message === undefined ? messageToken : message + messageToken;
+            message = message === undefined ? `${messageToken}` : `${message} ${messageToken}`;
         }
         if (message) {
             result[pipeOptions.messagePropertyName] = message;
         }
-        return [JSON.stringify(result)];
+        return [type, JSON.stringify(result)];
     };
 }
 
@@ -67,7 +66,7 @@ export type PickTopLevelPropertiesOptions = Pick<JsonPipeOptions, 'isTopLevelPro
 export function pickTopLevelProperties(obj: object, options: PickTopLevelPropertiesOptions = DEFAULT_JSON_PIPE_OPTIONS): Record<string, unknown> {
     const result: Record<string, unknown> = {};
     for (const [key, value] of Object.entries(obj)) {
-        if (typeof key === 'string' && options.isTopLevelProperty(key)) {
+        if (options.isTopLevelProperty(key)) {
             result[key] = value;
         }
     }
@@ -76,22 +75,20 @@ export function pickTopLevelProperties(obj: object, options: PickTopLevelPropert
 
 export type SafeJsonObjectOptions = Pick<JsonPipeOptions, 'maxRecursionLevel' | 'maxArrayLength' | 'maxObjectEntries' | 'isTopLevelProperty'>;
 
-export function buildSafeJsonValue(value: unknown,
-                                   options: SafeJsonObjectOptions = DEFAULT_JSON_PIPE_OPTIONS,
-                                   recursionLevel = 0,
-                                   visitedObjects = new Set<object>): object | null | string | undefined | number | boolean {
+/** A type with no 'symbol' & 'function': primitives + object. */
+type SimplifiedType = object | null | string | undefined | number | boolean | bigint;
+
+export function convertToSafeJson(value: unknown,
+                                  options: SafeJsonObjectOptions = DEFAULT_JSON_PIPE_OPTIONS,
+                                  recursionLevel = 0,
+                                  visitedObjects = new Set<object>): SimplifiedType {
 
     if (recursionLevel > options.maxRecursionLevel) {
         return DEPTH_LIMIT_VALUE;
     }
+    value = simplifyType(value);
     if (typeof value === 'string' || typeof value === 'boolean' || typeof value === 'number' || value === null || value === undefined) {
         return value;
-    }
-    if (typeof value === 'function') {
-        return FUNCTION_VALUE;
-    }
-    if (typeof value === 'symbol') {
-        return SYMBOL_VALUE;
     }
     if (visitedObjects.has(value)) {
         return CIRCULAR_REFERENCE_VALUE;
@@ -101,7 +98,7 @@ export function buildSafeJsonValue(value: unknown,
         if (value.length > options.maxArrayLength) {
             return ARRAY_LENGTH_LIMIT_VALUE.replace('$length', `${value.length}`);
         }
-        return value.map(v => buildSafeJsonValue(v, options, recursionLevel + 1, visitedObjects));
+        return value.map(v => convertToSafeJson(v, options, recursionLevel + 1, visitedObjects));
     }
     const entries = Object.entries(value);
     if (entries.length > options.maxObjectEntries) {
@@ -112,7 +109,48 @@ export function buildSafeJsonValue(value: unknown,
         if (recursionLevel === 0 && options.isTopLevelProperty(propertyName)) {
             continue;// Ignore the property. The property was moved to the top level.
         }
-        result [propertyName] = buildSafeJsonValue(propertyValue, options, recursionLevel + 1, visitedObjects);
+        result [propertyName] = convertToSafeJson(propertyValue, options, recursionLevel + 1, visitedObjects);
     }
     return result;
+}
+
+export function simplifyType(value: unknown): SimplifiedType {
+    if (value === null || value === undefined) {
+        return value;
+    }
+    switch (typeof value) {
+        case 'undefined':
+        case 'boolean':
+        case 'string':
+        case 'bigint':
+            return value;
+        case 'number':
+            if (isNaN(value)) {
+                return 'NaN';
+            } else if (value === Infinity) {
+                return 'Infinity';
+            } else if (value === -Infinity) {
+                return '-Infinity';
+            }
+            return value;
+        case 'function':
+            return FUNCTION_VALUE;
+        case 'symbol':
+            return SYMBOL_VALUE;
+        case 'object':
+            if (value instanceof Set) {
+                return [...value.keys()];
+            }
+            if (value instanceof Map) {
+                return Object.fromEntries([...value.entries()]);
+            }
+            if (value instanceof String || value instanceof Number || value instanceof Boolean) {
+                return value.valueOf();
+            }
+            if (value instanceof Date) {
+                return value.toISOString();
+            }
+            break;
+    }
+    return value;
 }
--- tests/json-pipe.jest.ts
+++ tests/json-pipe.jest.ts
@@ -1,10 +1,14 @@
+// noinspection JSPrimitiveTypeWrapperUsage
+
 import {describe, expect, it} from '@jest/globals';
 import {
-    buildSafeJsonValue,
     CIRCULAR_REFERENCE_VALUE,
+    convertToSafeJson,
+    createJsonPipe,
     DEFAULT_JSON_PIPE_OPTIONS,
     FUNCTION_VALUE,
-    pickTopLevelProperties
+    pickTopLevelProperties,
+    simplifyType
 } from '../src';
 
 describe('JsonPipe', () => {
@@ -34,50 +38,50 @@ describe('JsonPipe', () => {
         });
     });
 
-    describe('buildSafeValue', () => {
+    describe('convertToSafeJson', () => {
         it('correctly handles primitive types', () => {
             const obj = {1: 1, '2': 2, '3': '3', '4': null, '5': undefined, '6': true, '7': 1 / 3};
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual(obj);
         });
 
         it('correctly handles functions types', () => {
             const obj = {'foo': (): boolean => true};
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({'foo': FUNCTION_VALUE});
         });
 
         it('correctly handles undefined values', () => {
             const obj = {v: undefined};
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({});
         });
 
         it('filters out symbols as keys', () => {
             const obj = {stringProperty: '123'} as Record<symbol | string, unknown>;
             obj[symbolPropertyName] = '456';
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({stringProperty: '123'});
         });
 
         it('resolves circular references, direct reference', () => {
             const obj = {} as Record<string, object>;
             obj['ref'] = obj;
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({'ref': CIRCULAR_REFERENCE_VALUE});
         });
 
         it('resolves circular references, indirect reference', () => {
             const obj = {} as Record<string, object>;
             obj['ref'] = {obj};
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({'ref': {'obj': CIRCULAR_REFERENCE_VALUE}});
         });
 
         it('resolves circular references, reference in array', () => {
             const obj = {} as Record<string, object>;
             obj['array'] = [obj];
-            const result = buildSafeJsonValue(obj);
+            const result = convertToSafeJson(obj);
             expect(result).toEqual({'array': [CIRCULAR_REFERENCE_VALUE]});
         });
 
@@ -86,10 +90,10 @@ describe('JsonPipe', () => {
             for (let i = 0; i < DEFAULT_JSON_PIPE_OPTIONS.maxObjectEntries; i++) {
                 obj[`${i}`] = i;
             }
-            let result = buildSafeJsonValue(obj);
+            let result = convertToSafeJson(obj);
             expect(result).toEqual(obj);
 
-            result = buildSafeJsonValue({...obj, 'overflowField': 1});
+            result = convertToSafeJson({...obj, 'overflowField': 1});
             expect(result).toEqual(`[Object, entries: ${DEFAULT_JSON_PIPE_OPTIONS.maxObjectEntries + 1} ~]`);
         });
 
@@ -98,11 +102,11 @@ describe('JsonPipe', () => {
             for (let i = 0; i < DEFAULT_JSON_PIPE_OPTIONS.maxArrayLength; i++) {
                 array[i] = i;
             }
-            let result = buildSafeJsonValue(array);
+            let result = convertToSafeJson(array);
             expect(result).toEqual(array);
 
             array.push('overflow');
-            result = buildSafeJsonValue(array);
+            result = convertToSafeJson(array);
             expect(result).toEqual(`[Array, length: ${DEFAULT_JSON_PIPE_OPTIONS.maxArrayLength + 1} ~]`);
         });
 
@@ -115,11 +119,11 @@ describe('JsonPipe', () => {
                 object['child'] = child;
                 object = child;
             }
-            let result = buildSafeJsonValue(topLevelObject, options);
+            let result = convertToSafeJson(topLevelObject, options);
             expect(result).toEqual(topLevelObject);
 
             object['child'] = {};
-            result = buildSafeJsonValue(topLevelObject, options);
+            result = convertToSafeJson(topLevelObject, options);
             object['child'] = '[Depth limit ~]';
             expect(result).toEqual(topLevelObject);
         });
@@ -133,20 +137,139 @@ describe('JsonPipe', () => {
                 array.push(child);
                 array = child;
             }
-            let result = buildSafeJsonValue(topLevelArray, options);
+            let result = convertToSafeJson(topLevelArray, options);
             expect(result).toEqual(topLevelArray);
 
             array.push([]);
-            result = buildSafeJsonValue(topLevelArray, options);
+            result = convertToSafeJson(topLevelArray, options);
             array.pop();
             array.push('[Depth limit ~]');
             expect(result).toEqual(topLevelArray);
+        });
+
+        it('simplifyType is used', () => {
+            const result = convertToSafeJson(new Set([1, true, '3']));
+            expect(result).toEqual([1, true, '3']);
+        });
+    });
+
+    describe('simplifyType', () => {
+        it('Boolean parameter processed as a space separated boolean', () => {
+            const result = simplifyType(new Boolean(true));
+            expect(result).toBe(true);
+        });
+
+        it('Number parameter processed as a space separated number', () => {
+            const result = simplifyType(new Number(1));
+            expect(result).toBe(1);
+        });
+
+        it('Converts NaN to string', () => {
+            const result = simplifyType(NaN);
+            expect(result).toEqual('NaN');
+        });
+
+        it('Converts Infinity to string', () => {
+            const result = simplifyType(Infinity);
+            expect(result).toEqual('Infinity');
+        });
+
+        it('Converts -Infinity to string', () => {
+            const result = simplifyType(-Infinity);
+            expect(result).toEqual('-Infinity');
+        });
+        it('String object parameter processed as a space separated string', () => {
+            const result = simplifyType(new String('hello'));
+            expect(result).toBe('hello');
+        });
+
+        it('BigInt object parameter processed as a space separated string', () => {
+            const bigIntAsString = '1000000000000000000000000000000000000000000';
+            const result = simplifyType(BigInt(bigIntAsString));
+            expect(result).toBe(BigInt(bigIntAsString));
+        });
+
+        it('Date object parameter processed as an ISO formatted string', () => {
+            const result = simplifyType(new Date(0));
+            expect(result).toBe('1970-01-01T00:00:00.000Z');
+        });
+
+        it('Set object parameter processed as an array', () => {
+            const result = simplifyType(new Set(['1', '2', '3']));
+            expect(result).toEqual(['1', '2', '3']);
+        });
+
+        it('Map object parameter processed as a record', () => {
+            const result = simplifyType(new Map([['k1', 'v1'], ['k2', 'v2']]));
+            expect(result).toEqual({'k1': 'v1', 'k2': 'v2'});
         });
     });
 
     describe('createJsonPipe', () => {
         it('creates jsonPipe with default options', () => {
-            // TODO:
+            const pipe = createJsonPipe();
+            const result = pipe('log', {});
+            expect(result).toEqual(['log', `{"@message":"$1","$1":{}}`]);
+        });
+
+        it('empty log call results to an empty json with no message', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log');
+            expect(result).toEqual(['log', `{}`]);
+        });
+
+        it('a log call with an empty object results to a json with a message', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', {});
+            expect(result).toEqual(['log', `{"@message":"$1","$1":{}}`]);
+        });
+
+        it('a log call with an empty array results to a json with a message', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', []);
+            expect(result).toEqual(['log', `{"@message":"$1","$1":[]}`]);
+        });
+
+        it('null parameter processed as a space separated null', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', null);
+            expect(result).toEqual(['log', `{"@message":"Hello, null"}`]);
+        });
+
+        it('undefined parameter processed as a space separated undefined', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', undefined);
+            expect(result).toEqual(['log', `{"@message":"Hello, undefined"}`]);
+        });
+
+        it('boolean parameter processed as a space separated boolean', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', true);
+            expect(result).toEqual(['log', `{"@message":"Hello, true"}`]);
+        });
+
+        it('number parameter processed as a space separated number', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', 42);
+            expect(result).toEqual(['log', `{"@message":"Hello, 42"}`]);
+        });
+
+        it('string parameter processed as a space separated string', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', 'World');
+            expect(result).toEqual(['log', `{"@message":"Hello, World"}`]);
+        });
+
+        it('assigns continues indexes only for object properties', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', '1', {a: 'a'}, '2', 3, {'b': 'b'});
+            expect(result).toEqual(['log', `{"@message":"1 $1 2 3 $2","$1":{"a":"a"},"$2":{"b":"b"}}`]);
+        });
+
+        it('simplifyType is used', () => {
+            const pipe = createJsonPipe();
+            const result = pipe('log', 'Hello,', new Set([1, true, '3']));
+            expect(result).toEqual(['log', `{"@message":"Hello, $1","$1":[1,true,"3"]}`]);
         });
     });
 });
